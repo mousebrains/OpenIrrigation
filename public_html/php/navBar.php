54ab86a3789ff16cba85bfce2f1612a1f7d62624
Pat Welch <pat@mousebrains.com>
2017-03-22 09:48:38 -0700

    add menu items

--- public_html/php/navBar.php
+++ public_html/php/navBar.php
@@ -6,9 +6,21 @@ echo "
   <li id='topdropdown'>
     <a href='javascript:void(0)' id='topdropbtn'>&#9776;</a>
     <div id='top-dropdown-content'>
-      <a href='index.php'>Home</a>
+      <a href='index.php'>Manual Operations</a>
+      <a href='monitor.php'>Monitor Operations</a>
+      <a href='program.php'>Programs</a>
+      <a href='event.php'>Events</a>
+      <a href='programEvent.php'>Program Events</a>
+      <a href='station.php'>Stations</a>
       <a href='site.php'>Site Edit</a>
-      <a href='tpw.php'>TPW</a>
+      <a href='user.php'>User Edit</a>
+      <a href='userEMail.php'>EMail Edit</a>
+      <a href='controller.php'>Controller Edit</a>
+      <a href='sensor.php'>Decoder/Sensor Edit</a>
+      <a href='poc.php'>Point-of-connect Edit</a>
+      <a href='pocFlow.php'>Flow sensor Edit</a>
+      <a href='pocMV.php'>Master valve Edit</a>
+      <a href='pocPump.php'>Booster Pump Edit</a>
     </div>
   </li>
  </ul>
