f0183374a2e483fe4e9204cd8ba4c05f281b604c
Pat Welch <pat@mousebrains.com>
2017-05-24 08:34:49 -0700

    Add summary page for use on phone

--- public_html/php/navBar.php
+++ public_html/php/navBar.php
@@ -22,6 +22,7 @@ echo "  <div id='statusBlock'>Status info</div>
     <a href='javascript:void(0)' id='topdropbtn'>&#9776;</a>
     <div id='top-dropdown-content'>
       <a href='index.php'>Manual Operations</a>
+      <a href='smonitor.php'>Summary</a>
       <a href='monitor.php'>Monitor Operations</a>
       <a href='program.php'>Programs</a>
       <a href='programStation.php'>Program Station</a>
