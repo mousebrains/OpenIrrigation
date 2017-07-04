af08f29c78d2865a9132db55d3fdd1efd625c15b
Pat Welch <pat@mousebrains.com>
2017-07-04 14:21:09 -0700

    extracted database

--- database/extract.site.py
+++ database/extract.site.py
@@ -283,7 +283,7 @@ def getGroupStation(db, info):
 def getHistorical(db, info):
     sFields = OrderedDict([('sensor', info.sensor), ('program', info.program),
         ('pgmstn', info.pgmStn)])
-    fields = ['ton', 'toff', 'pgmdate', 'pre', 'peak', 'post', 'onCode', 'offCode']
+    fields = ['ton', 'toff', 'pgmdate', 'pre', 'peak', 'post', 'oncode', 'offcode']
     sql = "SELECT historical.sensor,ton,toff,program,pgmstn,pgmdate," \
 	+ "onLog.code As onCode,pre,peak,post," \
 	+ "offLog.code AS offCode" \
