017b9940213cb222376e951b5a7941096e3aae36
Pat Welch <pat@mousebrains.com>
2018-08-30 11:02:17 -0700

    Dump out, prune, and rsync database

---
+++ scripts/dumpDatabase.py
@@ -0,0 +1,232 @@
+#! /usr/bin/env python3
+#
+# Dump out the irrigation database and schema
+#
+import getpass
+import socket
+import sys
+import logging
+import logging.handlers
+import argparse
+import psycopg2
+import datetime
+from pathlib import Path
+import subprocess
+import smtplib
+from email.message import EmailMessage
+
+class BufferingSMTPHandler(logging.handlers.BufferingHandler):
+  def __init__(self, args):
+    logging.handlers.BufferingHandler.__init__(self, args.emailSize)
+    self.args = args
+
+  def flush(self):
+    print(self.args)
+    if len(self.buffer) > 0:
+      try:
+        msg = EmailMessage()
+        msg['From'] = self.args.sentFrom
+        msg['To'] = ','.join(self.args.email)
+        msg['Subject'] = self.args.subject
+        a = []
+        for record in self.buffer:
+          a.append(self.format(record))
+        msg.set_content("\r\n".join(a))
+        s = smtplib.SMTP(self.args.smtphost, self.args.smtpport)
+        s.send_message(msg)
+        s.quit()
+      except:
+        self.handleError(None)
+      self.buffer = []
+
+def doRsync(dirname, args, logger):
+  args = ["/usr/bin/rsync", "--archive", "--quiet", "--delete", dirname.as_posix(), args.rsyncto]
+  a = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  a = str(a.stdout, 'utf-8')
+  if len(a) > 0:
+    logger.warn(a)
+
+def saveSchema(dirname, dbname, logger):
+  fn = dirname.joinpath(dbname + ".schema.sql")
+  args = ["/usr/bin/pg_dump", 
+          "--file=" + fn.as_posix(), 
+          "--format=p", 
+          "--schema=public", 
+          "--schema-only", 
+          "--no-owner", 
+          "--no-privileges", 
+          "--dbname=" + dbname]
+  logger.info("Saving schema %s", fn)
+  a = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  a = str(a.stdout, 'utf-8')
+  if len(a) > 0:
+    logger.warn(a)
+
+def saveData(dirname, dbname, names, logger):
+  fn = dirname.joinpath(dbname + ".data.sql")
+  args = ["/usr/bin/pg_dump", 
+          "--file=" + fn.as_posix(), 
+          "--format=p", 
+          "--data-only", 
+          "--dbname=" + dbname]
+  for name in names:
+    args.append("--table=" + name)
+
+  logger.info("Saving data %s", fn)
+  a = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  a = str(a.stdout, 'utf-8')
+  if len(a) > 0:
+    logger.info(a)
+  
+
+def getTableNames(cur, ktables, logger):
+  cur.execute("SELECT table_name FROM information_schema.tables"
+            + " WHERE table_schema='public' and table_type!='VIEW';")
+  tables = set([tbl[0] for tbl in cur])
+  knames = set(ktables.keys())
+  for tbl in tables.difference(knames):
+    logger.warning('%s in database, but I do not know what to do with it', tbl)
+  for tbl in knames.difference(tables):
+    logger.warning('%s in known tables but not in database', tbl)
+
+  touse = {}
+  for tbl in tables.intersection(knames):
+    touse[tbl] = ktables[tbl]
+  return touse
+
+def saveMonthly(cur, tbl, dirname, tname, logger):
+  # Dump data through 21 days ago
+  now = datetime.datetime.utcnow();
+  maxdate = now - \
+            datetime.timedelta(days=21, 
+                               microseconds=now.microsecond, 
+                               seconds=now.second, 
+                               minutes=now.minute)
+
+  cur.execute("SELECT min({}),max({}) FROM {} WHERE {}<'{}';".format(
+              tname, tname, tbl, tname, maxdate))
+  (stime,etime) = cur.fetchone()
+
+  if stime is None: # No records
+    return
+
+  stime = stime.replace(microsecond=0, second=0, minute=0)
+  etime = etime.replace(microsecond=0, second=0, minute=0) + datetime.timedelta(hours=1)
+
+  fn = dirname.joinpath("{}.{}.{}.csv".format(tbl, stime.isoformat(), etime.isoformat()))
+  
+  logger.info('Dumping %s', fn)
+
+  # Copy rows that are before maxdate into a CSV file and delete them from
+  # the database
+
+  cur.execute("COPY (DELETE FROM {} ".format(tbl) \
+            + "WHERE {}>='{}' ".format(tname, stime.isoformat()) \
+            + "AND {}<'{}'".format(tname,etime.isoformat()) \
+            + " RETURNING *) " \
+            + " TO '{}' ".format(fn) \
+            + " WITH (FORMAT 'csv', HEADER TRUE);")
+
+knownTables = {
+  'event': 'skip',
+  'action': 'ignore', # Dynamic actions to be executed
+  'tableinfo': 'skip',
+  'sensor': 'skip',
+  'command': 'ignore', # Commands to  be executed
+  'simulate': 'skip',
+  'scheduler': 'ignore', # Scheduler times
+  'params': 'skip',
+  'weblist': 'skip',
+  'pgmdow': 'skip',
+  'eventdow': 'skip',
+  'crop': 'skip',
+  'soil': 'skip',
+  'usr': 'skip',
+  'email': 'skip',
+  'emailreports': 'skip',
+  'site': 'skip',
+  'controller': 'skip',
+  'poc': 'skip',
+  'pocmv': 'skip',
+  'pocpump': 'skip',
+  'pocflow': 'skip',
+  'station': 'skip',
+  'groupstation': 'skip',
+  'groups': 'skip',
+  'et': 'ignore', # Fetched from the outside world
+  'etannual': 'ignore', # Built from et
+  'etstation': 'skip',
+  'program': 'skip',
+  'pgmstn': 'skip',
+  'currentlog': 'monthly',
+  'sensorlog': 'monthly',
+  'teelog': 'monthly',
+  'numberlog': 'monthly',
+  'peelog': 'monthly',
+  'twolog': 'monthly',
+  'zeelog': 'monthly',
+  'errorlog': 'monthly',
+  'versionlog': 'monthly',
+  'historical': 'historical',
+  }
+
+timeKeys = {'monthly': 'timestamp', 'historical': 'toff'}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--db', help='ET database name', required=True)
+parser.add_argument('--dir', help='Directory to dump data to', required=True)
+parser.add_argument('--log', help='logfile, if not specified use the console')
+parser.add_argument('--email', help='Who to email log to', nargs='*')
+parser.add_argument('--sentFrom', help='Who is sending the email', 
+                    default= getpass.getuser() + "@" + socket.gethostname())
+parser.add_argument('--smtphost', help='SMTP hostname', default='localhost')
+parser.add_argument('--smtpport', help='SMTP port', default=25)
+parser.add_argument('--emailSize', help='Maximum email size', default=100000)
+parser.add_argument('--subject', help='email subject', default=sys.argv[0])
+parser.add_argument('--rsyncto', help='target of rsync')
+parser.add_argument('--verbose', help='Increase verbosity level', action='store_true')
+args = parser.parse_args()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+if args.log is None:
+    ch = logging.StreamHandler()
+else:
+    ch = logging.FileHandler(args.log)
+
+ch.setLevel(logging.DEBUG if args.verbose else logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s: %(threadName)s:%(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+if args.email is not None:
+  mh = BufferingSMTPHandler(args)
+  mh.setLevel(logging.DEBUG)
+  logger.addHandler(mh)
+
+dirname = Path(args.dir).resolve()
+
+saveSchema(dirname, args.db, logger)
+toSave = []
+
+with psycopg2.connect("dbname=" + args.db) as conn, conn.cursor() as cur:
+  tables = getTableNames(cur, knownTables, logger)
+  for key in tables:
+    val = tables[key]
+    if val in timeKeys:
+      saveMonthly(cur, key, dirname, timeKeys[val], logger)
+      conn.commit()
+    elif val == 'skip': # Will be saved as part of pg_dump
+      toSave.append(key)
+    elif val == 'ignore': # Just ignore
+      pass # Skip is dumped elsewhere, ignore is ignored totally
+    else:
+      logger.error('Unsupported action, %s for %s', val, key)
+
+saveData(dirname, args.db, toSave, logger)
+
+if args.rsyncto is not None:
+  doRsync(dirname, args, logger)
