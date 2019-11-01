2da993fd26957e16dff2729f6105f3d2cc5aef39
Pat Welch <pat@mousebrains.com>
2019-11-01 08:39:29 -0700

    favicon support

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta http-equiv='Content-Language' content='en'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
+<link rel='icon' href='/favicon.png' sizes="32x32">
 <link rel='stylesheet' type='text/css' href='css/irrigation.css'>
 <script src="js/jquery.min.js"></script>
 <title>Wild Iris Irrigation</title>
