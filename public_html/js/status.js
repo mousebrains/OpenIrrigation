c11ee91da7d84a474c4131588c0d00d4d4a00c5f
Pat Welch <pat@mousebrains.com>
2019-11-06 15:38:17 -0800

    Clean up legacy variables and switch from let to var in function

--- public_html/js/status.js
+++ public_html/js/status.js
@@ -1,14 +1,10 @@
 var statusControllers;
 var statusPOCs;
-var statusCurrent;
-var statusSensor;
-var statusOn;
-var statusPending;
 
 if (typeof(EventSource) != "undefined") {
 	var statusSource = new EventSource("status.php");
 	statusSource.onmessage = function(event) {
-		let data = JSON.parse(event.data);
+		var data = JSON.parse(event.data);
 		if ('controllers' in data) {statusControllers = data['controllers'];}
 		if ('pocs' in data) {statusPOCs = data['pocs'];}
 		if ('simulation' in data) {
@@ -19,14 +15,14 @@ if (typeof(EventSource) != "undefined") {
 			}
 		}
                 if ('ctl' in data) { // Current
-			let t = new Date(data['tcurrent'] * 1000);
+			var t = new Date(data['tcurrent'] * 1000);
 			$('#statusCurrent').html(statusControllers[data['ctl']]
 				+ ' ' + data['volts'] + 'V ' + data['mamps'] + 'mA'
 				+ ' ' + t.toTimeString().substr(0,9)
 			);
 		}
                 if ('poc' in data) { // Flow
-			let t = new Date(data['tflow'] * 1000);
+			var t = new Date(data['tflow'] * 1000);
 			$('#statusFlow').html(statusPOCs[data['poc']]
 				+ ' ' + data['flow'] + 'GPM'
 				+ ' ' + t.toTimeString().substr(0,9)
