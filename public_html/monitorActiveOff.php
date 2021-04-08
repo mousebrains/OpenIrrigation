f6fdf0c481cd7008e4632ca92e9babbf0a2ff902
Pat Welch <pat@mousebrains.com>
2021-04-08 13:30:06 -0700

    Move mkMsg and dbMsg into DB1

--- public_html/monitorActiveOff.php
+++ public_html/monitorActiveOff.php
@@ -3,14 +3,10 @@
 
 require_once 'php/DB1.php';
 
-function mkMsg(bool $flag, string $msg) {return json_encode(['success'=>$flag, 'message'=>$msg]);}
-function dbMsg($db, string $msg) {return mkMsg(false, $msg . ", " . $db->getError());}
+if (empty($_POST['id'])) exit($db->mkMsg(false, "No ID supplied."));
 
-if (empty($_POST['id'])) exit(mkMsg(false, "No ID supplied."));
 $id = $_POST['id'];
 
-if ($db->query('SELECT manual_off(?);', [$id])) {
-	exit(mkMsg(true, "Turned Valve $id off"));
-}
-echo dbMsg($db, "Failed to turn valve($id) off");
+if ($db->query('SELECT manual_off(?);', [$id])) exit($db->mkMsg(true, "Turned Valve $id off"));
+echo $db->dbMsg("Failed to turn valve($id) off");
 ?>
